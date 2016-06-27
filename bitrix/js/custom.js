acc8c78aa835f8103101451a5e4e43d3bd9e8450
Helga13 <oliatrubina@yandex.ru>
2016-06-27 17:58:48 +0300

    cat-inner cart

--- bitrix/js/custom.js
+++ bitrix/js/custom.js
@@ -5,6 +5,10 @@ $(document).ready(function(){
 	
 	$('.child_container').siblings('a.parent').addClass('after');	
 	
+$(function() {
+    $('.section_item').matchHeight();
+	$('.articles-list.sections .item.iblock').matchHeight();
+});	
 	
 	
 	
